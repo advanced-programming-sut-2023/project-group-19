6ebf14ec5c22af7a2cfd98aa2155c84374d3127f
“armin”‏ <arminprg83@gmail.com>
2023-04-20 22:25:17 +0430

    last commit before cloning

--- src/controller/MainMenuController.java
+++ src/controller/MainMenuController.java
@@ -4,15 +4,8 @@ import model.*;
 import java.util.Scanner;
 
 public class MainMenuController {
-    public static String currentMenu;
-
-    public static void run(Scanner scanner){
-
-    }
-    public void getMenuToEnterIt(){
-
-    }
-    public String printTextToShowState(){
-        return null ;
+    public static void logout(){
+        User.setCurrentUser(null);
     }
 }
+
--- src/view/MainMenu.java
+++ src/view/MainMenu.java
@@ -1,12 +1,13 @@
 package view;
 
+import controller.MainMenuController;
 import view.Commands.MainMenuCommands;
 
 import java.util.Scanner;
 import java.util.regex.Matcher;
 
 public class MainMenu {
-    public static void run(Scanner scanner){
+    public static void run(Scanner scanner) throws InterruptedException {
         String command ;
         Matcher matcher ;
         while (true){
@@ -15,6 +16,10 @@ public class MainMenu {
             if((matcher = MainMenuCommands.getMatcher(command,MainMenuCommands.ENTER_PROFILE_MENU)) != null){
                 System.out.println("entered profile menu successfully");
                 ProfileMenu.run(scanner);
+            }else if(MainMenuCommands.getMatcher(command,MainMenuCommands.LOGOUT) != null){
+                MainMenuController.logout();
+                System.out.println("logged out");
+                return;
             }
         }
     }
