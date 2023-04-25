02ad4fe421818dfd0eb62077077ead7341911e7a
Dorsa-Sharifi <dorsa.sharifi@gmail.com>
2023-04-25 14:00:30 +0330

    TradeMenuCommands completely added.

--- src/main/java/view/Commands/TradeMenuCommands.java
+++ src/main/java/view/Commands/TradeMenuCommands.java
@@ -2,11 +2,13 @@ package view.Commands;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public enum TradeMenuCommands {
+    TRADE("\\s*trade\\s+"),
     SEND_REQUEST_RESOURCE_TYPE_CHECK("(?<resourceType>(-t\\s+(?=\"+)[^-]+)\"{1}|(-t\\s+(?=\"{0})[^\\s-]+))"),
     SEND_REQUEST_RESOURCE_AMOUNT_CHECK("(?<resourceAmount>-a\\s+\\d+)"),
     SEND_REQUEST_RESOURCE_PRICE_CHECK("?<resourcePrice>-p\\s+\\d+"),
     SEND_REQUEST_RESOURCE_MESSAGE_CHECK("?<resourceMessage>(-m\\s+(?=\"+)[^-]+)\"{1}|(-m\\s+(?=\"{0})[^\\s-]+)"),
     SHOW_TRADE_LIST("\\s*trade\\s+list"),
+    TRADE_ACCEPTED("\\s*trade\\s+accept\\s+"),
     TRADE_ACCEPTED_ID_CHECK("?<id>(-i\\s+(?=\"+)[^-]+)\"{1}|(-i\\s+(?=\"{0})[^\\s-]+))"),
     TRADE_ACCEPTED_MESSAGE_CHECK("?<tradeMessage>(-m\\s+(?=\"+)[^-]+)\"{1}|(-m\\s+(?=\"{0})[^\\s-]+)"),
     SHOW_TRADE_HISTORY("\\s*trade\\s+history");
