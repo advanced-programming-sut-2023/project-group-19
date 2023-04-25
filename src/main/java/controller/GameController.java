25b16aa61d28df1f8fdb284f5ee6ade957222815
Dorsa-Sharifi <dorsa.sharifi@gmail.com>
2023-04-25 13:57:08 +0330

    TradeMenuCommands added

---
+++ src/main/java/controller/GameController.java
@@ -0,0 +1,5 @@
+package controller;
+
+public class GameController {
+    //TODO: SWITCHING TO SHOP MENU & TRADE MENU
+}
--- src/main/java/controller/TradeController.java
+++ src/main/java/controller/TradeController.java
@@ -3,11 +3,20 @@ package controller;
 import model.*;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
 
 public class TradeController {
     public static String currentMenu;
 
     public static void run(Scanner scanner) {
+        String command;
+        Matcher matcher;
+        while (true){
+            command=scanner.nextLine();
+            if (true){
+
+            }
+        }
 
     }
 
