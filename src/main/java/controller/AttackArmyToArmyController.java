b61d6dc8252a21b97f40aac4e89ea0f7a2bb36d5
“armin”‏ <arminprg83@gmail.com>
2023-05-04 17:27:38 +0430

    complete the archer

--- src/main/java/controller/AttackArmyToArmyController.java
+++ src/main/java/controller/AttackArmyToArmyController.java
@@ -66,7 +66,6 @@ public class AttackArmyToArmyController {
             }
         }
         return convertedArrayList ;
-
     }
 
     private static ArrayList<Empire> getEmpiers(ArrayList<Army> armies){
