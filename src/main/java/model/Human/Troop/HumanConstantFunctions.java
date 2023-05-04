86fee3e9353c06bb5e48bb3137ee6854b98be9c5
“Arian” <arian09337332108@gmail.com>
2023-05-04 12:16:55 +0330

    merging with arian

---
+++ src/main/java/model/Human/Troop/HumanConstantFunctions.java
@@ -0,0 +1,15 @@
+package model.Human.Troop;
+
+import java.util.List;
+
+public interface HumanConstantFunctions {
+    public int maxHp();
+
+    public int hp();
+    public int speed();
+    public int getCurrentX();
+    public int getCurrentY();
+    public int restOfMoves();
+    public List<Integer> myPath();
+
+}
---
+++ src/main/java/model/Human/Troop/Tunneler.java
@@ -0,0 +1,116 @@
+package model.Human.Troop;
+
+import model.Empire;
+import model.Human.Human;
+import model.Human.Names;
+
+import java.util.List;
+
+public class Tunneler extends Army implements HumanConstantFunctions{
+    public Tunneler(Empire government) {
+        super(government);
+    }
+    private Names names;
+    private int hp;
+    private int maxHp;
+    private int speed;
+    private int defencePower;
+    private int attackPower;
+    private int attackRange;
+    public int xCoordinate;
+    public int yCoordinate;
+    public int restOfMoves;
+    public Names typeOfArmy;
+    public List<Integer> myPath;
+    public Names getNames() {
+        return names;
+    }
+
+    public void setNames(Names names) {
+        this.names = names;
+    }
+
+    @Override
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void setMaxHp(int maxHp) {
+        this.maxHp = maxHp;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getDefencePower() {
+        return defencePower;
+    }
+
+    public void setDefencePower(int defencePower) {
+        this.defencePower = defencePower;
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+    public void setAttackPower(int attackPower) {
+        this.attackPower = attackPower;
+    }
+
+    public int getAttackRange() {
+        return attackRange;
+    }
+
+    public void setAttackRange(int attackRange) {
+        this.attackRange = attackRange;
+    }
+
+    public Names getTypeOfArmy() {
+        return typeOfArmy;
+    }
+
+    public void Tunneler(int x , int y) {
+        typeOfArmy = Names.STANDING_AMRY;
+        names = Names.TUNNELER;
+        xCoordinate = x;
+        yCoordinate = y;
+        speed = 7;
+    }
+
+    @Override
+    public int maxHp() {
+        return maxHp;
+    }
+
+    @Override
+    public int hp() {
+        return hp;
+    }
+
+    @Override
+    public int speed() {
+        return speed;
+    }
+
+    @Override
+    public int getCurrentX() {
+        return xCoordinate;
+    }
+
+    @Override
+    public int getCurrentY() {
+        return yCoordinate;
+    }
+
+    @Override
+    public int restOfMoves() {
+        return restOfMoves;
+    }
+
+    @Override
+    public List<Integer> myPath() {
+        return myPath;
+    }
+}
