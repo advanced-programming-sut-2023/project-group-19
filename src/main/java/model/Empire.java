358735ddf14f809b8b2a5b2cd20c4bfd2d5b5da3
“Arian” <arian09337332108@gmail.com>
2023-05-04 19:44:00 +0330

    FINISHED MY JOB PART :))) FINALLY I GUESS I DID ALL I COULD IN THIS PART AND ALL FUNCTIONS ARE READY TO BE USED

--- src/main/java/model/Empire.java
+++ src/main/java/model/Empire.java
@@ -1215,19 +1215,4 @@ public class Empire {
         FunctionBuildingController.transformWoodToBow();
         FunctionBuildingController.transformWoodToSpearOrPeak();
     }
-//TODO CHECK this part with dorsa
-
-//    public void setPopularityBasedOnReligion() {
-//        for (int x = 0; x < Map.mapSize; x++) {
-//            for (int y = 0; y < Map.mapSize; y++) {
-//                if (Map.getBuildingMap()[x][y].get(0).getName().equals(Names.BIG_CHURCH)
-//                        && Map.getBuildingMap()[x][y].get(0).getOwner().equals(Manage.getCurrentEmpire())) {
-//                    Manage.getCurrentEmpire().setPopularity(Manage.getCurrentEmpire().getPopularity() + 2);
-//                } else if (Map.getBuildingMap()[x][y].get(0).getName().equals(Names.SMALL_CHURCH)
-//                        && Map.getBuildingMap()[x][y].get(0).getOwner().equals(Manage.getCurrentEmpire())) {
-//                    Manage.getCurrentEmpire().setPopularity(Manage.getCurrentEmpire().getPopularity() + 1);
-//                }
-//            }
-//        }
-//    }
 }
