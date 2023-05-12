8a38c85e40b77db69886931c0966af2dff52da88
“Arian” <arian09337332108@gmail.com>
2023-05-12 11:05:30 +0330

    fixed a problem in empire

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,7 +24,7 @@ public class Main {
 //        System.out.println(BuildingCommands.getMatcher(command, BuildingCommands.BUILDING_COMMANDS_DROP_BUILDING));
 
 
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
 //        LoginMenu.run(scanner);
 
 
@@ -35,42 +35,42 @@ public class Main {
 //        Map.CreateMap(200);
 //        empireMenu.run(scanner);
 
-        User newUser2 = new User("user5", "aa", "reza", "a", "1", "1", 1);
-        User newUser = new User("user6", "aa", "ali", "a", "1", "1", 1);
-        Empire empire = new Empire();
-        Manage.setCurrentEmpire(empire);
-        Empire newEmpire = new Empire();
-        newEmpire.setUser(newUser2);
-        Map.CreateMap(200);
-        empire.setUser(newUser);
-        empire.setEngineerCount(60000000);
-        empire.setWorkerCount(500000);
-        empire.setWoodCount(20000000);
-        empire.setStoneCount(300000);
-        empire.setGoldCount(1000000);
-        empire.setWoodCount(1000000);
-        empire.setOilAmount(1000000);
-        empire.setIronCount(10000000);
-        empire.setSwordCount(1000000);
-        empire.setSpearCount(100000000);
-        empire.setHorseCount(10000000);
-        empire.setLeatherArmourCount(1000000000);
-        empire.setMetalArmourCount(100000000);
-        empire.setMaceCount(100000000);
-        empire.setBowCount(1000000000);
-        empire.setPeakCount(100000000);
-        Wall wall = new Wall(empire);
-        wall.smallWall();
-        wall.setHp(200);
-
-        ArchersAndThrowers archer = new ArchersAndThrowers(newEmpire);
-        archer.archer(3 , 5);
-        Map.troopMap[3][5].add(archer);
-        Map.buildingMap[5][5].add(wall);
-        empire.setPeasantCount(100000000);
-        System.out.println(GameController.enemyInRange(5 , 5));
-        BuildingMenu buildingMenu = new BuildingMenu();
-        buildingMenu.run(scanner);
+//        User newUser2 = new User("user5", "aa", "reza", "a", "1", "1", 1);
+//        User newUser = new User("user6", "aa", "ali", "a", "1", "1", 1);
+//        Empire empire = new Empire();
+//        Manage.setCurrentEmpire(empire);
+//        Empire newEmpire = new Empire();
+//        newEmpire.setUser(newUser2);
+//        Map.CreateMap(200);
+//        empire.setUser(newUser);
+//        empire.setEngineerCount(60000000);
+//        empire.setWorkerCount(500000);
+//        empire.setWoodCount(20000000);
+//        empire.setStoneCount(300000);
+//        empire.setGoldCount(1000000);
+//        empire.setWoodCount(1000000);
+//        empire.setOilAmount(1000000);
+//        empire.setIronCount(10000000);
+//        empire.setSwordCount(1000000);
+//        empire.setSpearCount(100000000);
+//        empire.setHorseCount(10000000);
+//        empire.setLeatherArmourCount(1000000000);
+//        empire.setMetalArmourCount(100000000);
+//        empire.setMaceCount(100000000);
+//        empire.setBowCount(1000000000);
+//        empire.setPeakCount(100000000);
+//        Wall wall = new Wall(empire);
+//        wall.smallWall();
+//        wall.setHp(200);
+//
+//        ArchersAndThrowers archer = new ArchersAndThrowers(newEmpire);
+//        archer.archer(3 , 5);
+//        Map.troopMap[3][5].add(archer);
+//        Map.buildingMap[5][5].add(wall);
+//        empire.setPeasantCount(100000000);
+//        System.out.println(GameController.enemyInRange(5 , 5));
+//        BuildingMenu buildingMenu = new BuildingMenu();
+//        buildingMenu.run(scanner);
 //        Industry woodCutter = new Industry(empire);
 //        woodCutter.woodCutter();
 //        Map.buildingMap[0][0].add(woodCutter);
