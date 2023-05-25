06895d4494cd7b699c6095457ac3802fc03415a3
“Arian” <arian09337332108@gmail.com>
2023-05-25 18:25:41 +0330

    creating map

--- src/main/java/view/TileManager.java
+++ src/main/java/view/TileManager.java
@@ -15,7 +15,9 @@ import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import model.Empire;
+import model.Human.Troop.ArchersAndThrowers;
 import model.Human.Troop.Army;
+import model.Human.Troop.Soldiers;
 import model.Manage;
 import org.w3c.dom.ls.LSOutput;
 import view.Model.NewButton;
@@ -34,14 +36,13 @@ public class TileManager extends Application {
     public int avgDamage;
     public int avgSpeed;
     public TilePane view  = new TilePane();
+    public Pane pane = new Pane();
     public int avgHp;
 
     @Override
     public void start(Stage stage) throws Exception {
-        TilePane tilePane = new TilePane();
 //        tilePane.setLayoutX(-100);
 //        tilePane.setLayoutY(-100);
-        tilePane.setTileAlignment(Pos.CENTER);
 //        tilePane.setPrefColumns(100);
 //        tilePane.setMaxWidth(10000);
         ArrayList<Node> list = new ArrayList<>();
@@ -51,7 +52,13 @@ public class TileManager extends Application {
                 setEventHandler(newButton);
                 newButton.setPrefSize(51 , 54);
                 newButton.setFocusTraversable(false);
-
+                Soldiers soldiers = new Soldiers(Manage.getCurrentEmpire());
+                soldiers.BlackMonk(5 , 16);
+                ArchersAndThrowers archersAndThrowers = new ArchersAndThrowers(Manage.getCurrentEmpire());
+                archersAndThrowers.archer(5 , 16);
+                newButton.getArmy().add(archersAndThrowers );
+                newButton.getArmy().add(soldiers );
+                newButton.setText(String.valueOf(j * 100 + i));
                 list.add(newButton);
             }
         }
@@ -59,13 +66,15 @@ public class TileManager extends Application {
 //         width  = 1530
 //         height = 800
 
-
         for(int u = 0 ; u < 16 ; u++){
             for(int g = 0 ;  g < 30 ; g++ ){
 //                ((Button)list.get((u + 3) * 100 + (g + 10))).setBackground(new Background(new BackgroundImage(
 //                        new Image("C:\\Users\\F1\\Desktop\\chert\\ProjectGroup19\\src\\main\\resources\\image\\cegla2.jpg") ,
 //                        BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
-                view.getChildren().add(list.get((u + 3) * 100 + (g + 10)));
+                Button button = (Button) list.get((u + 3) * 100 + (g + 10));
+                button.setLayoutX(g * 51.2);
+                button.setLayoutY(u * 54);
+                pane.getChildren().add(list.get((u + 3) * 100 + (g + 10)));
             }
         }
 
@@ -88,7 +97,8 @@ public class TileManager extends Application {
                 }
             }
         });
-        Scene scene = new Scene(view , width-50 , height-50);
+        pane.getChildren().add(view);
+        Scene scene = new Scene(pane , width-50 , height-50);
 
         stage.setTitle("Tile Pane");
         stage.setScene(scene);
@@ -109,40 +119,43 @@ public class TileManager extends Application {
             public void handle(MouseEvent mouseEvent) {
                 newButton.setStyle(null);
                 showCellData.setText("");
-                view.getChildren().remove(showCellData);
+                pane.getChildren().remove(showCellData);
             }
         };
         EventHandler<MouseEvent> event3 = new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
-//                getCellData(newButton);
-//                PointerInfo a = MouseInfo.getPointerInfo();
-//                Point b = a.getLocation();
-//                int x = (int) b.getX();
-//                int y = (int) b.getY();
-//                StringBuilder stringBuilder = new StringBuilder();
-//                stringBuilder.append("AVG Hp : " + avgHp + '\n'  + "AVG Damage : " + avgDamage + '\n' +
-//                        "AVG Speed : " + avgSpeed + '\n' + cellArmyNameType);
-//                showCellData.setText(stringBuilder.toString());
-//                showCellData.setX(x);
-//                showCellData.setY(y);
-                showCellData.setText("ali");
-                showCellData.setX(500);
-                showCellData.setY(500);
-                view.getChildren().add(showCellData);
+                getCellData(newButton);
+                PointerInfo a = MouseInfo.getPointerInfo();
+                Point b = a.getLocation();
+                int x = (int) b.getX();
+                int y = (int) b.getY() - 50;
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append("AVG Hp : " + avgHp + '\n'  + "AVG Damage : " + avgDamage + '\n' +
+                        "AVG Speed : " + avgSpeed + '\n');
+                for(int i = 0 ; i < cellArmyNameType.size() ; i++){
+                    stringBuilder.append(cellArmyNameType.get(i) + " ");
+                }
+                showCellData.setText(stringBuilder.toString());
+                showCellData.setX(x);
+                showCellData.setY(y);
+                if(showCellData != null && !pane.getChildren().contains(showCellData))
+                    pane.getChildren().add(showCellData);
             }
         };
 //        newButton.setOnMouseClicked(event);
         newButton.setOnMouseClicked(event);
         newButton.setOnMouseExited(event2);
-        newButton.setOnMouseEntered(event3);
+        newButton.setOnMouseMoved(event3);
+//        newButton.setOnMouse(event3);
     }
     public void getCellData(NewButton newButton){
         cellArmyNameType.clear();
         int damage = 0;
         int hp = 0;
         int speed = 0;
-        for(int i = 0 ;  i < newButton.getArmy().size() ; i++){
+        int i;
+        for(i = 0 ;  i < newButton.getArmy().size() ; i++){
             if(!cellArmyNameType.contains(newButton.getArmy().get(i).getNames().getName())){
                 cellArmyNameType.add(newButton.getArmy().get(i).getNames().getName());
             }
@@ -150,5 +163,8 @@ public class TileManager extends Application {
             hp += newButton.getArmy().get(i).getHp();
             speed += newButton.getArmy().get(i).getSpeed();
         }
+        avgHp = hp/i;
+        avgSpeed = speed/i;
+        avgDamage = damage/i;
     }
 }
