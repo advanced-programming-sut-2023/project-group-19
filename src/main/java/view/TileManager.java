6c7feadce957ff7a0f2b65999b02504f7a38623d
Dorsa-Sharifi <dorsa.sharifi@gmail.com>
2023-05-25 15:11:16 +0330

    some changes in the tile manager.

--- src/main/java/view/TileManager.java
+++ src/main/java/view/TileManager.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
@@ -27,41 +28,47 @@ public class TileManager extends Application {
         tilePane.setPrefColumns(100);
         tilePane.setMaxWidth(10000);
         ArrayList<Node> list = new ArrayList<>();
+        TilePane view  = new TilePane();
         for(int j = 0 ; j < 100 ; j++) {
             for (int i = 0; i < 100; i++) {
 
                 NewButton newButton = new NewButton(j , i);
-
-                EventHandler<MouseEvent> event = new EventHandler<MouseEvent>() {
+                EventHandler<MouseEvent> event1 = new EventHandler<MouseEvent>() {
                     @Override
                     public void handle(MouseEvent mouseEvent) {
-                        System.out.println("x : " + newButton.getX() );
-                        System.out.println("y : " + newButton.getY());
-//                        button.setStyle("-fx-background-color: red;" + "-fx-text-fill: white");
+                        if (mouseEvent.getEventType().getName().equals(MouseEvent.MOUSE_DRAGGED.getName())){
+//                            moveTheMap(view,list);
+                            System.out.println("Dragged");
+                        }else {
+                            System.out.println("Clicked");
+                        }
                     }
                 };
+//                EventHandler<MouseEvent> event = new EventHandler<MouseEvent>() {
+//                    @Override
+//                    public void handle(MouseEvent mouseEvent) {
+//                        System.out.println("x : " + newButton.getX() );
+//                        System.out.println("y : " + newButton.getY());
+////                        button.setStyle("-fx-background-color: red;" + "-fx-text-fill: white");
+//                    }
+//                };
                 newButton.setPrefSize(51 , 54);
                 newButton.setFocusTraversable(false);
-                newButton.setOnMouseClicked(event);
+                newButton.setOnMouseDragged(event1);
+                newButton.setOnMouseClicked(event1);
                 list.add(newButton);
             }
         }
 //         width  = 1530
 //         height = 800
-
-        TilePane view  = new TilePane();
         for(int u = 0 ; u < 16 ; u++){
             for(int g = 0 ;  g < 30 ; g++ ){
                 ((Button)list.get((u + 3) * 100 + (g + 10))).setBackground(new Background(new BackgroundImage(
-                        new Image("C:\\Users\\F1\\Desktop\\chert\\ProjectGroup19\\src\\main\\resources\\image\\cegla2.jpg") ,
+                        new Image(TileManager.class.getResource("/image/cegla2.jpg").toExternalForm()) ,
                         BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
                 view.getChildren().add(list.get((u + 3) * 100 + (g + 10)));
             }
         }
-
-//        view.setBackground(new Background( new BackgroundImage( new Image(Game.class.getResource("/image/cegla2.jpg").toExternalForm()) ,
-//                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
-
         Dimension resolution = Toolkit.getDefaultToolkit().getScreenSize();
         double width = resolution.getWidth();
         double height = resolution.getHeight();
@@ -71,5 +78,8 @@ public class TileManager extends Application {
         stage.setScene(scene);
         stage.show();
         stage.setFullScreen(true);
+    }
+
+    private static void moveTheMap(TilePane view ,ArrayList<Node> list ){
     }
 }
