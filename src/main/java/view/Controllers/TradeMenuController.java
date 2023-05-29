2ac14e00e69917b22ae1d2847cf9d39d7ff71d6b
Dorsa-Sharifi <dorsa.sharifi@gmail.com>
2023-05-29 09:15:29 +0330

    Start of TradeMenu Graphic.

---
+++ src/main/java/view/Controllers/TradeMenuController.java
@@ -0,0 +1,28 @@
+package view.Controllers;
+
+import controller.TradeController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import view.Main;
+import view.TradeMenu;
+
+public class TradeMenuController extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        TradeController tradeController = new TradeController();
+        Main.stage = stage;
+        Pane pane = new Pane();
+        pane.setPrefSize(1000,900);
+        designTradeMenu(tradeController,pane,stage);
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.show();
+        stage.setFullScreen(true);
+    }
+
+    private void designTradeMenu(TradeController tradeController, Pane pane, Stage stage) {
+        
+    }
+}
