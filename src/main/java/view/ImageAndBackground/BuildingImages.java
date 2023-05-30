104167e4a5867cd3e8eeaac0085fec5556edc063
“Arian” <arian09337332108@gmail.com>
2023-05-30 03:51:13 +0330

    adding the building images load function

---
+++ src/main/java/view/ImageAndBackground/BuildingImages.java
@@ -0,0 +1,249 @@
+package view.ImageAndBackground;
+
+import javafx.scene.image.Image;
+
+public class BuildingImages {
+    public Image apothecary;
+    public Image appleFarm;
+    public Image armory;
+    public Image armourer;
+    public Image bakery;
+    public Image ballista;
+    public Image barracks;
+    public Image bigChurch;
+    public Image bigStoneGateHouse;
+    public Image blacksmith;
+    public Image brewery;
+    public Image cagedWarDogs;
+    public Image crenulatedWall;
+    public Image dairyFarm;
+    public Image defenceTurret;
+    public Image drawBridge;
+    public Image engineerGuild;
+    public Image fletcher;
+    public Image garden;
+    public Image hopsFarm;
+    public Image hovel;
+    public Image inn;
+    public Image ironMine;
+    public Image killingPit;
+    public Image lookoutTower;
+    public Image lowWall;
+    public Image mangonel;
+    public Image market;
+    public Image oilSmelter;
+    public Image oxTheater;
+    public Image pitchDitch;
+    public Image pitchRig;
+    public Image poleTurner;
+    public Image premierTurret;
+    public Image quarry;
+    public Image roundTower;
+    public Image smallChurch;
+    public Image smallStoneGatehouse;
+    public Image squareTower;
+    public Image stable;
+    public Image stockpile;
+    public Image stoneWall;
+    public Image tanner;
+    public Image tortureBuilding;
+    public Image tunnelersGuild;
+    public Image wallStair;
+    public Image wheatFarm;
+    public Image woodCutter;
+    public void loadImage(){
+
+    }
+
+    public Image getApothecary() {
+        return apothecary;
+    }
+
+    public Image getAppleFarm() {
+        return appleFarm;
+    }
+
+    public Image getArmory() {
+        return armory;
+    }
+
+    public Image getArmourer() {
+        return armourer;
+    }
+
+    public Image getBakery() {
+        return bakery;
+    }
+
+    public Image getBallista() {
+        return ballista;
+    }
+
+    public Image getBarracks() {
+        return barracks;
+    }
+
+    public Image getBigChurch() {
+        return bigChurch;
+    }
+
+    public Image getBigStoneGateHouse() {
+        return bigStoneGateHouse;
+    }
+
+    public Image getBlacksmith() {
+        return blacksmith;
+    }
+
+    public Image getBrewery() {
+        return brewery;
+    }
+
+    public Image getCagedWarDogs() {
+        return cagedWarDogs;
+    }
+
+    public Image getCrenulatedWall() {
+        return crenulatedWall;
+    }
+
+    public Image getDairyFarm() {
+        return dairyFarm;
+    }
+
+    public Image getDefenceTurret() {
+        return defenceTurret;
+    }
+
+    public Image getDrawBridge() {
+        return drawBridge;
+    }
+
+    public Image getEngineerGuild() {
+        return engineerGuild;
+    }
+
+    public Image getFletcher() {
+        return fletcher;
+    }
+
+    public Image getGarden() {
+        return garden;
+    }
+
+    public Image getHopsFarm() {
+        return hopsFarm;
+    }
+
+    public Image getHovel() {
+        return hovel;
+    }
+
+    public Image getInn() {
+        return inn;
+    }
+
+    public Image getIronMine() {
+        return ironMine;
+    }
+
+    public Image getKillingPit() {
+        return killingPit;
+    }
+
+    public Image getLookoutTower() {
+        return lookoutTower;
+    }
+
+    public Image getLowWall() {
+        return lowWall;
+    }
+
+    public Image getMangonel() {
+        return mangonel;
+    }
+
+    public Image getMarket() {
+        return market;
+    }
+
+    public Image getOilSmelter() {
+        return oilSmelter;
+    }
+
+    public Image getOxTheater() {
+        return oxTheater;
+    }
+
+    public Image getPitchDitch() {
+        return pitchDitch;
+    }
+
+    public Image getPitchRig() {
+        return pitchRig;
+    }
+
+    public Image getPoleTurner() {
+        return poleTurner;
+    }
+
+    public Image getPremierTurret() {
+        return premierTurret;
+    }
+
+    public Image getQuarry() {
+        return quarry;
+    }
+
+    public Image getRoundTower() {
+        return roundTower;
+    }
+
+    public Image getSmallChurch() {
+        return smallChurch;
+    }
+
+    public Image getSmallStoneGatehouse() {
+        return smallStoneGatehouse;
+    }
+
+    public Image getSquareTower() {
+        return squareTower;
+    }
+
+    public Image getStable() {
+        return stable;
+    }
+
+    public Image getStockpile() {
+        return stockpile;
+    }
+
+    public Image getStoneWall() {
+        return stoneWall;
+    }
+
+    public Image getTanner() {
+        return tanner;
+    }
+
+    public Image getTortureBuilding() {
+        return tortureBuilding;
+    }
+
+    public Image getTunnelersGuild() {
+        return tunnelersGuild;
+    }
+
+    public Image getWallStair() {
+        return wallStair;
+    }
+
+    public Image getWheatFarm() {
+        return wheatFarm;
+    }
+
+    public Image getWoodCutter() {
+        return woodCutter;
+    }
+}
