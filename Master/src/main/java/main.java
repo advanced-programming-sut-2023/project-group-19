34084a5f08bd3f5500ae97b3d723d27c250136da
“Arian” <arian09337332108@gmail.com>
2023-06-28 01:49:10 +0330

    commiting

---
+++ Master/src/main/java/main.java
@@ -0,0 +1,14 @@
+import com.google.gson.Gson;
+import masterServer.MasterServer;
+
+import java.util.HashMap;
+
+public class main {
+    public static void main(String[] args){
+       MasterServer masterServer =  new MasterServer(8080);
+
+
+
+
+    }
+}
